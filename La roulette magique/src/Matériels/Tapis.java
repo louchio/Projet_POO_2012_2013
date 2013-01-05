a3f6a05d5574fdc8feb214399410382db7ebff85
Ali NAJEM <ali.najem@hotmail.fr>
2013-01-05 22:15:34 +0100

    Modifier la structure de cette classe

--- La roulette magique/src/Matériels/Tapis.java
+++ La roulette magique/src/Matériels/Tapis.java
@@ -5,15 +5,20 @@ import Cases_du_tapis.*;
 public class Tapis {
 	private Roue roue;
 	private int nbre_cases;
-	private Numero liste_cases[];
-	private Gagne gagne;
+	private Numero liste_numeros[];
+	private Passe passe;
+	private Manque manque;
+	private Impair impair;
+	private Pair pair;
 
 	public Tapis(Roue roue, int nbre_cases) {
 		this.roue = roue;
 		this.nbre_cases = nbre_cases;
 		// ........................
 		// créer les cases... (avec les couleurs..)
-		// et remplire liste_cases
+		// et remplire liste_numeros
+		// manque, passe.. 
+		
 		
 	}
 
