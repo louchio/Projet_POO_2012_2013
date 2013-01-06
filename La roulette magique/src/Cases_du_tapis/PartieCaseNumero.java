b5cabe8b86f8460f17e66782c5949e2233ad746c
Ali NAJEM <ali.najem@hotmail.fr>
2013-01-06 03:07:22 +0100

    Ajouter un nouvel attribut "indice", et deplacer l'attribut "couleur" dans la classe "Mise"

--- La roulette magique/src/Cases_du_tapis/PartieCaseNumero.java
+++ La roulette magique/src/Cases_du_tapis/PartieCaseNumero.java
@@ -3,19 +3,24 @@ package Cases_du_tapis;
 import java.awt.Color;
 
 public class PartieCaseNumero extends Mise{	
-	private Color couleur;
-
-	public PartieCaseNumero(Color couleur, int coef, int mise) {
-		super(coef, mise);
-		this.couleur = couleur;
+	private int indice;				// l'indice de la partie de la case
+	
+	public PartieCaseNumero(int indice, int coef, Color couleur) {
+		super(coef, couleur);
+		this.indice = indice;
+	}
+	
+	public PartieCaseNumero(int indice, int coef, int mise, Color couleur) {
+		super(coef, mise, couleur);
+		this.indice = indice;	
 	}
 
-	public Color getCouleur() {
-		return couleur;
+	public int getIndice() {
+		return indice;
 	}
 
-	public void setCouleur(Color couleur) {
-		this.couleur = couleur;
+	public void setIndice(int indice) {
+		this.indice = indice;
 	}
-		
+	
 }
