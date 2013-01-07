3ad2039417f6aeff08cdd02ff593fab70adf0648
Ali NAJEM <ali.najem@hotmail.fr>
2013-01-07 10:37:28 +0100

    Arraylist Impair

--- La roulette magique/src/cases_du_tapis/Impair.java
+++ La roulette magique/src/cases_du_tapis/Impair.java
@@ -1,13 +1,19 @@
 package cases_du_tapis;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Impair extends Mise{
-	private int liste_impair[];
+	private ArrayList liste_impair;
 	
 	public Impair(int coef, Color couleur) {
 		super(coef, couleur);
-		// initialiser liste impair
+
+		liste_impair = new ArrayList<Integer>();
+		liste_impair.add(0);
+		liste_impair.add(1);
+		
+		//liste_impair = {0, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
 	}
 	
 	public Impair(int coef, int mise, Color couleur) {
@@ -16,11 +22,11 @@ public class Impair extends Mise{
 	}
 	
 	
-	public int[] getListe_impair() {
+	public ArrayList getListe_impair() {
 		return liste_impair;
 	}
 
-	public void setListe_impair(int[] liste_impair) {
+	public void setListe_impair(ArrayList liste_impair) {
 		this.liste_impair = liste_impair;
 	}	
 }
